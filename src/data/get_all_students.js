7172ffce62b27314ac7665cc20647b1d267b248d
Joshua1gd <joshua1gd@yahoo.com>
2019-02-08 15:47:37 -0800

    Completed displaying student data from dummy data"

---
+++ src/data/get_all_students.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'Steve B.',
+        course: 'Math 101',
+        grade: 76
+    },
+    {
+        id: 2,
+        name: 'Joe C.',
+        course: 'Pol Sci',
+        grade: 88
+    },
+    {
+        id: 3,
+        name: 'Al F.',
+        course: 'Bio',
+        grade: 93
+    },
+    {
+        id: 4,
+        name: 'William 2',
+        course: 'Writing 12',
+        grade: 82
+    },
+];
