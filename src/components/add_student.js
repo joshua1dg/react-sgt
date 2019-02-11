e589ea0e3a1557284710dba4e154e5700f08fed6
Joshua1gd <joshua1gd@yahoo.com>
2019-02-11 14:43:41 -0800

    added add and delete student functionality

--- src/components/add_student.js
+++ src/components/add_student.js
@@ -9,32 +9,25 @@ class AddStudent extends Component {
     };
 
     handleKeyPress = (event) => {
-        console.log('Event Name: ', event.target.name);
-        console.log('Event Value: ', event.target.value);
 
-        switch(event.target.name){
-            case 'name':
-                this.setState({
-                    name: event.target.value
-                });
-                break;
-            case 'course':
-                this.setState({
-                    course: event.target.value
-                });
-                break;
-            case 'grade':
-                this.setState({
-                    grade: event.target.value
-                });
-                break;
-        }
+        this.setState({
+            [event.target.name] : event.target.value
+        });
     }
 
     handleSubmit = (event) => {
         event.preventDefault()
-        console.log('Form Submitted ', this.state)
+        this.props.add(this.state);
+        this.resetForm();
     }
+
+    resetForm = () => {
+      this.setState({
+          name: '',
+          course: '',
+          grade: ''
+      });
+    };
 
     render(){
         const{name, course, grade} = this.state;
@@ -43,25 +36,25 @@ class AddStudent extends Component {
             <form onSubmit={this.handleSubmit}>
                 <div className="row">
                     <div className="col input-field s10 offset-s1">
-                        <input onChange={this.handleKeyPress} name='name' id='name' type="text" value={name}/>
+                        <input onChange={this.handleKeyPress} name='name' id='name' type="text" value={name} autoComplete='off'/>
                         <label htmlFor="name">Name</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col input-field s10 offset-s1">
-                        <input onChange={this.handleKeyPress} name='course' id='course' type="text"  value={course}/>
+                        <input onChange={this.handleKeyPress} name='course' id='course' type="text"  value={course} autoComplete='off'/>
                         <label htmlFor="course">Course</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col input-field s10 offset-s1">
-                        <input onChange={this.handleKeyPress} name='grade' id='grade' type="text" value={grade}/>
+                        <input onChange={this.handleKeyPress} name='grade' id='grade' type="text" value={grade} autoComplete='off'/>
                         <label htmlFor="grade">Grade</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col s6 center">
-                        <button type='button' className='btn red darken-2 waves-effect waves-light'>Clear</button>
+                        <button type='button' onClick={this.resetForm} className='btn red darken-2 waves-effect waves-light'>Clear</button>
                     </div>
                     <div className="col s6">
                         <button className='btn green darken-2 center'>Add</button>
